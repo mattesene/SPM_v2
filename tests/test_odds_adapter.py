5affade03c20481c43d4cd2debd879842412620b
mattesene <matteo.senestrari@gmail.com>
2026-08-20 21:19:30 +0200

    fix: align OOS adapter tests with BacktestObservation

--- tests/test_odds_adapter.py
+++ tests/test_odds_adapter.py
@@ -5,28 +5,22 @@ from spm.backtest.odds_adapter import attach_odds_to_oos
 
 
 def test_selected_oos_match_receives_odds():
-    class M:
+    class O:
         date = date(2025, 1, 1)
         home_team = "Milan"
         away_team = "Roma"
-
-    class O:
-        match = M()
         actual_draw = True
         selected = True
 
-    result = attach_odds_to_oos([O()], [DrawOdds(date(2025, 1, 1), "Milan", "Roma", 3.2, "test")])
+    result = attach_odds_to_oos([O()], [DrawOdds(date(2025, 1, 1), "Milan", "Roma", 3.2)])
     assert result[0].draw_odds == 3.2
 
 
 def test_unselected_oos_match_does_not_require_odds():
-    class M:
+    class O:
         date = date(2025, 1, 1)
         home_team = "Milan"
         away_team = "Roma"
-
-    class O:
-        match = M()
         actual_draw = False
         selected = False
 
